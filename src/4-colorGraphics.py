9fd9c23959fea8c88e75c7550ed086c848a4c49e
Andreas Kugel <andreas.kugel@web.de>
2018-10-04 10:43:48 +0200

    update

--- src/4-colorGraphics.py
+++ src/4-colorGraphics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import imageio
+from skimage import color
 
 # line drawing
 def dl(x,y,len=0,dir=-1,col=128):
@@ -51,6 +52,8 @@ I[:,:,1].fill(128)    # green
 I[:,:,2].fill(128)  # blue
 
 
+
+
 # do some line drawing
 x = N//2
 y = x
@@ -89,6 +92,52 @@ plt.axis('off')
 plt.show()
 
 imageio.imwrite("imgrgb.jpg",I)
+
+print("Colors can be defined differently\n\
+RGB is based upon display physics, but it's hard to guess\n\
+color from values. HSV is better in this repsect\n")
+
+# use HSV colors, see https://en.wikipedia.org/wiki/HSL_and_HSV
+
+# image dimensions
+N = 256
+# empty image. needs additional dimentsion for color
+I = np.empty((N,N,3), dtype = np.uint8)
+
+# generate HSV image
+v = 200 # default V
+for r in range(N):
+    for c in range(N):
+        I[r,c] = (r,c,v) 
+
+# convert to RGB
+I2 = color.hsv2rgb(I)        
+
+# make a scaled RGB image with color resolution RGB:565
+J = np.empty((N,N,3), dtype = np.uint8)
+for g in range(2**6):
+    for b in range(2**5):
+        for r in range(2**5):
+            i = g*2**10 + b*2**5 + r # pixel index
+            J[i//256,i%256] = (r*2**3,g*2**2,b*2**3) # rescale colors
+
+# create a figure
+f = plt.figure()
+p1 = f.add_subplot(121)
+# setting the climit is important!
+p1.imshow(J,clim=(0,255))
+p1.axis('off')
+p1.set_title("RGB Colors")
+
+p2 = f.add_subplot(122)
+# setting the climit is important!
+p2.imshow(I2,clim=(0,255))
+p2.axis('off')
+p2.set_title("HSV Colors")
+ 
+plt.show()
+
+
 
 print("Done")
 plt.close()
